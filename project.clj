07baac865da60069879a42d0aa6cd2605b1dafe2
makinoshi <masa.kinoshita02@gmail.com>
2019-11-14 08:10:35 +0900

    Version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toyokumo/kintone-clj "0.4.0-SNAPSHOT"
+(defproject toyokumo/kintone-clj "0.4.0"
   :description "A kintone SDK for Clojure and ClojureScript"
   :url "https://github.com/toyokumo/kintone-clj"
   :license {:name "Apache, Version 2.0"
