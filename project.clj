ad71ef241c24115300ddc1127cfdd27a2dc7f449
liquidz <liquidz.uo@gmail.com>
2020-05-21 15:25:07 +0900

    Version 0.5.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toyokumo/kintone-client "0.5.1"
+(defproject toyokumo/kintone-client "0.5.2-SNAPSHOT"
   :description "A kintone client for Clojure and ClojureScript"
   :url "https://github.com/toyokumo/kintone-client"
   :license {:name "Apache, Version 2.0"
